32d74c8e2159e73e1fa9b5a393630ac27ea93908
Ryota <n.ryo950911@gmail.com>
2021-06-30 22:44:30 +0900

    0630commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence:true
+  validates :self_introduction, length:{ maximum:500 }
 
-  enum gender:{man:0, woman:1}
+  enum gender:{男性:0, 女性:1}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'top/index'
+  root 'top#index'
 
   resources :users, only:[:show]
 end
