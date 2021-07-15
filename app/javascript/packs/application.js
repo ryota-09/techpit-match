49e6aac7b240cbfe97e50f574f5bd45836cfcb8b
Ryota <n.ryo950911@gmail.com>
2021-07-15 21:34:38 +0900

    0715commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import'hammerjs';
 
 import '../stylesheets/application';
-import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all';
 
 
 // This file is automatically compiled by Webpack, along with any other files
@@ -17,7 +17,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 require("src/profile_image_upload");
-require("src/swipe")
+require("src/swipe");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -2,37 +2,35 @@ if(location.pathname == "/users") {
   $(function(){
     let allCards = document.querySelectorAll('.swipe--card');
 
-    let swipeContainer = document.querySelector('.swipe');
-
-    function initCards(){
-      allCards.forEach(function(card, index){
-        card.style.zindex = allCards.length - index;
-        card.style.transform = 'scale(' + (20 - index)/ 20 + ')translateY(-' + 30*index + 'px)';
-        card.style.opacity = (10 - index)/ 10;
+    function initCards() {
+      allCards.forEach(function (card, index) {
+        card.style.zIndex = allCards.length - index;
+        card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+        card.style.opacity = (10 - index) / 10;
       });
     }
 
-    initCards()
+    initCards();
 
-    allCards.forEach(function(el){
-      let hammertime = new Hanmmer(el);
+    allCards.forEach(function (el) {
+      let hammertime = new Hammer(el);
 
       hammertime.on('pan', function(event){
-        if(event.deltaX === 0)return;
-        if(event.center.x === 0 && event.center.y === 0)return;
+        if (event.deltaX === 0) return;
+        if (event.center.x === 0 && event.center.y === 0) return;
 
         el.classList.add('moving');
 
         swipeContainer.classList.toggle('swipe_like', event.deltaX > 0);
         swipeContainer.classList.toggle('swipe_dislike', event.deltaX < 0);
 
-        let xMulti = event.deltaX*0.03;
-        let yMulti = event.deltaY/80;
-        let rotate = xMulti*yMukti;
+        let xMulti = event.deltaX * 0.03;
+        let yMulti = event.deltaY / 80;
+        let rotate = xMulti * yMulti;
 
-        event.target.style.transform = 'translate(' + event.deltaX + 'px,' + event.deltaY + 'px)rotate(' + rotete + 'deg)';
-      })
-    })
+        event.target.style.transform = 'translate(' + event.deltaX + 'px,' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
+      });
+    });
   });
 
 }
