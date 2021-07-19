7834e61a8634a310869d98348b65554a4f7e4dd9
Ryota <n.ryo950911@gmail.com>
2021-07-19 22:26:45 +0900

    0719commit

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -5,7 +5,9 @@ if(location.pathname == "/users") {
     let swipeContainer = document.querySelector('.swipe');
 
     function initCards() {
-      allCards.forEach(function (card, index) {
+      let newCards = document.querySelectorAll('.swipe--card:not(.removed)');
+
+      newCards.forEach(function (card, index) {
         card.style.zIndex = allCards.length - index;
         card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
         card.style.opacity = (10 - index) / 10;
@@ -31,6 +33,33 @@ if(location.pathname == "/users") {
         let rotate = xMulti * yMulti;
 
         event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
+      });
+
+      hammertime.on('panend',function(event){
+        el.classList.remove('moving');
+        swipeContainer.classList.remove('swipe_like');
+        swipeContainer.classList.remove('swipe_dislike');
+
+        let moveOutWidth = document.body.clientWidth;
+
+        let keep = Math.abs(event.deltaX) < 200
+        event.target.classList.toggle('removed',!keep);
+
+        if(keep){
+          event.target.style.transform = '';
+        } else {
+          let endX = Math.max(Math.abs(event.velocityX)*moveOutWidth, moveOutWidth) + 100;
+          let toX = event.deltaX > 0 ? endX:-endX;
+          let endY = Math.abs(event.velocityY)*moveOutWidth;
+          let toY = event.deltaY > 0 ? endY:-endY;
+          let xMulti = event.deltaX*0.03;
+          let yMulti = event.deltaY / 80;
+          let rotate = xMulti*yMulti;
+
+          event.target.style.transform = 'translate(' + toX + 'px,' + (toY + event.deltaY) + 'px)rotate(' + rotate + 'deg)';
+
+          initCards();
+        }
       });
     });
   });
