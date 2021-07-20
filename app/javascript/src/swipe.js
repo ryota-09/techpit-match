8b0a68d37e6762326b00e4889ea0bd9673248853
Ryota <n.ryo950911@gmail.com>
2021-07-20 22:27:55 +0900

    0720commit

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -12,6 +12,10 @@ if(location.pathname == "/users") {
         card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
         card.style.opacity = (10 - index) / 10;
       });
+
+      if(newCards.length == 0){
+        $(".no-user").addClass("is-active");
+      }
     }
 
     initCards();
@@ -62,6 +66,35 @@ if(location.pathname == "/users") {
         }
       });
     });
-  });
 
+    function createButtonListener(reaction){
+
+      let cards = document.querySelectorAll('.swipe--card:not(.removed)');
+
+      if(!cards.length)return false;
+
+      let moveOutWith = document.body.clientWidth*2;
+
+      let card = cards[0];
+      card.classList.add('removed');
+
+      if(reaction == "like"){
+        card.style.transform = 'translate(' + moveOutWith + 'px, -100px)rotate(-30deg)';
+      } else {
+        card.style.transform = 'translate(-' + moveOutWith + 'px, -100px)rotate(30deg)';
+      }
+
+      initCards();
+    }
+  
+    $('#like').on('click', function(){
+      createButtonListener("like");
+    })
+  
+    $('#dislike').on('click', function(){
+      createButtonListener("dislike");
+    })
+
+
+  });
 }
